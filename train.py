03ff3f3200f24297aaf047cd5eeed29d3d8f0273
qkhanh39 <khanhtruong3904@gmail.com>
2024-11-30 15:09:47 +0700

    Add project

---
+++ train.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OrdinalEncoder
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+data_raw = pd.read_csv('Data/drug.csv')
+
+
+X = data_raw.drop('Drug', axis=1).values
+y = data_raw.Drug.values
+
+columns_to_encode = [1, 2, 3]
+
+encoder = OrdinalEncoder()
+X[:, columns_to_encode] = encoder.fit_transform(X[:, columns_to_encode])
+y = encoder.fit_transform(y.reshape(-1, 1)).reshape(-1)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+model = RandomForestClassifier(n_estimators=3, random_state=42)
+model.fit(X_train, y_train)
+
+y_pred = model.predict(X_test)
+
+print('Accuracy:', accuracy_score(y_test, y_pred))
+
+with open("./Results/metrics.txt", "w") as f:
+    f.write(str(accuracy_score(y_test, y_pred)))
+
+import pickle
+with open('./Model/model.pkl', 'wb') as f:
+    pickle.dump(model, f)
+
+
